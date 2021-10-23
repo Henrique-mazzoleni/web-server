1e44e6180ba021d5a75ec3f5cb0933ff3b9fbddd
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-23 08:57:33 +0200

    Dynamic loading implemented

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log("The frontside js is loaded");
--- src/app.js
+++ src/app.js
@@ -4,7 +4,32 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Wetterapp",
+    h1: "Weather",
+    name: "Henrique",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    h1: "About Me",
+    name: "Henrique Mazzoleni",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Hilfe",
+    h1: "Help Page",
+    name: "Mazzoleni",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
