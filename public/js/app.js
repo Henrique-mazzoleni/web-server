ae4e8bf3f34cd9493e160da48549c3f215bbf097
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-23 22:02:09 +0200

    added final styling and functionality

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,23 @@ console.log("Client side js is loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.getElementById("message-1");
+const messageTwo = document.getElementById("message-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
 
+  messageOne.textContent = "Loading";
+  messageTwo.textContent = "";
   fetch(`/weather?search=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.forecast);
-        console.log(data.location);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
