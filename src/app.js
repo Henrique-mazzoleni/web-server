5106988d290418c647531ccafeb9277234f25ba6
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-23 11:32:58 +0200

    partials for header and footer implemented

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths to express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewPath = path.join(__dirname, "../templates");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
